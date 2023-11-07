e28d4fad8171b25b7ac9ee632c3abf74ecd7dd00
khs-alt <aa_aa_aa_11@naver.com>
2023-11-07 11:22:17 +0900

    merge for fisrt

--- main.go
+++ main.go
@@ -5,11 +5,8 @@ import (
 )
 
 func main() {
-<<<<<<< HEAD
-=======
 	fmt.Printf("%s", "for study Github")
 	fmt.Println("changed!!")
->>>>>>> second
 	fmt.Printf("%s", "for study Github")
 	fmt.Println("changed!!")
 }
